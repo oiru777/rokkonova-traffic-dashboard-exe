e3b3a2f094b1fa50c3a34a53fec120eaa96e00cd
oiru777 <warisara11037@gmail.com>
2025-09-08 13:09:31 +0900

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
-    port: 5173
+    port: 5173,
+    strictPort: true, // 5173 が使えなければエラーにする（5174に逃げない）
+    open: false       // 起動時にブラウザを開かない
   }
 })
